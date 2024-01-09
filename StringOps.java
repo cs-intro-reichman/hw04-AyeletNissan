b9dab7d680c1c68f7b6107bbcbeb761f93f0f7e3
AyeletNissan <ayelet.nissan@post.runi.ac.il>
2024-01-09 13:45:04 +0200

    add

--- StringOps.java
+++ StringOps.java
@@ -22,7 +22,6 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        System.out.println(camelCase("Hello World"));
     }
 
     public static String capVowelsLowRest (String string) {
@@ -43,17 +42,29 @@ public class StringOps {
     public static String camelCase (String string) {
         String help = "";
         boolean afterspace = false;
+        boolean firstspace = false; 
+
         for(int i=0; i<string.length(); i++){ 
-            
-             if(string.charAt(i)==' ') afterspace=true;
+            if(string.charAt(i)==' '){
+                if(i==0) firstspace = true;
+                afterspace=true;
+            } 
             else if(i==0){
                 if (string.charAt(i)<=90 && string.charAt(i)>=65) help+=(char)(string.charAt(i)+32) ;
                 else help+=string.charAt(i);
             }
             else if(afterspace==true && string.charAt(i)!=' '){
-                if (string.charAt(i)<=90 && string.charAt(i)>=65) help+=string.charAt(i);
-                else help+=(char)(string.charAt(i)-32);
-                afterspace=false;
+                if(firstspace==false){
+                   if (string.charAt(i)<=90 && string.charAt(i)>=65) help+=string.charAt(i);
+                   else help+=(char)(string.charAt(i)-32);
+                   afterspace=false; 
+                } 
+                else if (firstspace=true){
+                    if (string.charAt(i)<=90 && string.charAt(i)>=65) help+=(char)(string.charAt(i)+32) ;
+                    else help+=string.charAt(i);
+                    firstspace=false;
+                    afterspace=false;
+                }
             }
             else if (string.charAt(i)<=90 && string.charAt(i)>=65) help+=(char)(string.charAt(i)+32);
             else help+=string.charAt(i);
@@ -72,7 +83,6 @@ public class StringOps {
         int r=0;
 
         for(int j=0; j<end.length; j++){
-            //for(int r=0; r<string.length(); r++)
             while (isin==false) {
                 if (string.charAt(r)==chr){
                     end[j]=r;
